3af5dbf7d5e6fafc428d8e79f54b51e93b8f30b8
Adel-Elkenany <131967154+Adel-Elkenany@users.noreply.github.com>
2025-12-11 17:41:17 +0200

    feat: add product-service and update configurations

--- apps/product-service/src/main.ts
+++ apps/product-service/src/main.ts
@@ -1,21 +1,48 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
-import * as path from 'path';
+import cors from 'cors';
+// // Global handlers to avoid process exit on unhandled promise rejections / exceptions
+// process.on('unhandledRejection', (reason) => {
+//     console.error('Unhandled Rejection at:', reason);
+// });
+// process.on('uncaughtException', (err) => {
+//     console.error('Uncaught Exception thrown:', err);
+// });
+import { errorMiddleware } from '@packages/error-handler/error-middleware';
+import cookieParser from 'cookie-parser';
+// import swaggerUi from "swagger-ui-express"
+// const swaggerDocument = require('./swagger-output.json');
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  allowedHeaders: ["Authorization", "Content-Type"],
+  credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to product-service!' });
+app.get('/', (req, res) => {
+  res.send({ 'message': 'Hello API' });
 });
 
-const port = process.env.PORT || 3333;
+// Swagger Documentation
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.get("/docs-json", (req, res) => {
+//     res.json(swaggerDocument);
+// });
+
+// Routes 
+// app.use('/api', router);
+
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 6002;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Auth service is running at http://localhost:${port}/api`);
+  console.log(`Swagger docs available at http://localhost:${port}/docs`);
 });
-server.on('error', console.error);
+
+server.on('error', (err) => {
+  console.error('Server error:', err);
+});
